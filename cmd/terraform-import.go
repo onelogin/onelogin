910fe2d874532725d0b1e9675099cfd93c179a33
Dominick Caponi <d.caponi1@gmail.com>
2020-05-11 11:11:24 -0700

    finishes creating main.tf after import

--- cmd/terraform-import.go
+++ cmd/terraform-import.go
@@ -1,7 +1,10 @@
 package cmd
 
 import (
+	"bufio"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -28,106 +31,173 @@ func init() {
 	})
 }
 
-type TerraformResource struct {
-	ImportCommand *exec.Cmd   // the terraform import command for the resource
-	Content       []byte      // the []byte representation of main.tf contents
-	Name          string      // app.Name-app.ID with all special chars removed
-	Type          string      // terraform resource type (e.g. onelogin_saml_apps)
-	Resource      interface{} // the resource itself
+type TerraformPlaceholder struct {
+	ImportCommand *exec.Cmd // the terraform import command for the resource
+	Content       []byte    // the []byte representation of main.tf contents
+	Name          string    // app.Name-app.ID with all special chars removed
+	Type          string    // terraform resource type (e.g. onelogin_saml_apps)
 }
 
-/// what if we just peel apart the resource and parse sub-resources separately
-
-func (tfResource *TerraformResource) CreateResourceFileOutput() {
-	head := fmt.Sprintf("resource %s %s {\n", tfResource.Type, tfResource.Name)
-	tfResource.Content = append(tfResource.Content, []byte(head)...)
-	tfResource.Content = append(tfResource.Content, resourceBaseToHCL(tfResource.Resource)...)
-	tfResource.Content = append(tfResource.Content, fmt.Sprintf("}\n\n")...)
-}
-
-func (tfResource *TerraformResource) CreateTerraformImportCommand() {
-	arg1 := fmt.Sprintf("%s.%s", tfResource.Type, tfResource.Name)
+func (placeholder *TerraformPlaceholder) InitializeTerraformImport() {
+	placeholder.Content = append(placeholder.Content, []byte(fmt.Sprintf("resource %s %s {}\n", placeholder.Type, placeholder.Name))...)
+	arg1 := fmt.Sprintf("%s.%s", placeholder.Type, placeholder.Name)
 	pos := strings.Index(arg1, "-")
 	id := arg1[pos+1 : len(arg1)]
-	tfResource.ImportCommand = exec.Command("terraform", "import", arg1, id)
+	placeholder.ImportCommand = exec.Command("terraform", "import", arg1, id)
 }
 
 func terraformImport(cmd *cobra.Command, args []string) {
 	fmt.Println("Terraform Import!")
+	fmt.Println("Collecting Apps from OneLogin...")
 
 	allApps := getAllApps()
 
-	f := setUpTerraformFile()
+	fmt.Printf("This will import %d apps. Do you want to continue? (y/n): ", len(allApps))
+	input := bufio.NewScanner(os.Stdin)
+	input.Scan()
+	ans := input.Text()
+	if ans != "y" {
+		os.Exit(0)
+	}
+
+	f := setUpTerraformFile("main")
 	defer f.Close()
 
-	var resourcesToBootstrap []TerraformResource
-	for _, app := range allApps {
-		resource := createResourceRepresentation(app)
-		resourcesToBootstrap = append(resourcesToBootstrap, resource)
-	}
-	writeInitialMainTF(f, resourcesToBootstrap)
-	importTFState(resourcesToBootstrap)
-	// read the .tfstate file as json and update each resource in main.tf
+	placeholders := make([]TerraformPlaceholder, len(allApps))
 
+	for i, app := range allApps {
+		placeholder := TerraformPlaceholder{}
+		switch *app.AuthMethod {
+		case 1, 8:
+			placeholder.Type = "onelogin_oidc_apps"
+		case 2:
+			placeholder.Type = "onelogin_saml_apps"
+		default:
+			placeholder.Type = "onelogin_apps"
+		}
+		placeholder.Name = fmt.Sprintf("%s-%d", toSnakeCase(replaceSpecialChar(*app.Name, "")), *app.ID)
+		placeholder.InitializeTerraformImport()
+		placeholders[i] = placeholder
+	}
+	state := importTFState(f, placeholders)
+	writeFinalMainTF(f, state.Resources)
 }
 
-func importTFState(resources []TerraformResource) {
-	exec.Command("terraform", "init").Run()
+type InstanceData struct {
+	AllowAssumedSignin *bool                     `json:"allow_assumed_signin"`
+	ConnectorID        *int                      `json:"connector_id"`
+	Description        *string                   `json:"description"`
+	Name               *string                   `json:"name"`
+	Notes              *string                   `json:"notes"`
+	Visible            *bool                     `json:"visible"`
+	Provisioning       []models.AppProvisioning  `json:"provisioning"`
+	Parameters         []models.AppParameters    `json:"parameters"`
+	Configuration      []models.AppConfiguration `json:"configuration"`
+}
+
+type ResourceInstance struct {
+	Data InstanceData `json:"attributes"`
+}
+
+type TerraformResource struct {
+	Content   []byte
+	Name      string             `json:"name"`
+	Type      string             `json:"type"`
+	Instances []ResourceInstance `json:"instances"`
+}
+
+type TerraformState struct {
+	Resources []TerraformResource `json:"resources"`
+}
+
+func writeFinalMainTF(f *os.File, resources []TerraformResource) {
+	log.Println("Assembling main.tf...")
+	buffer := []byte("provider onelogin {}\n\n")
 	for _, resource := range resources {
-		if err := resource.ImportCommand.Run(); err != nil {
-			fmt.Println("ERROR IN TF", err)
+		for _, instance := range resource.Instances {
+			resource.Content = append(resource.Content, []byte(fmt.Sprintf("resource %s %s {\n", resource.Type, resource.Name))...)
+			resource.Content = append(resource.Content, resourceBaseToHCL(instance.Data, 1)...)
+			resource.Content = append(resource.Content, []byte("}\n\n")...)
+		}
+		buffer = append(buffer, resource.Content...)
+	}
+	f.WriteAt(buffer, 0)
+}
+
+func readTFStateJSON() TerraformState {
+	log.Println("Collecting State from State File")
+
+	path, _ := os.Getwd()
+	p := filepath.Join(path, "/terraform.tfstate")
+	data, err := ioutil.ReadFile(p)
+	if err != nil {
+		log.Fatal("Unable to Read tfstate")
+	}
+	v := TerraformState{}
+
+	if err := json.Unmarshal(data, &v); err != nil {
+		log.Fatal("Unable to Translate tfstate in Memory")
+	}
+	return v
+}
+
+func importTFState(f *os.File, placeholders []TerraformPlaceholder) TerraformState {
+	log.Println("Creating Terraform Import File...")
+	buffer := []byte("provider onelogin {}\n\n")
+	for _, placeholder := range placeholders {
+		buffer = append(buffer, placeholder.Content...)
+	}
+	if _, err := f.Write(buffer); err != nil {
+		log.Fatal("Problem creating import file", err)
+	}
+
+	log.Println("Initializing Terraform with 'terraform init'...")
+	exec.Command("terraform", "init").Run()
+	for i, placeholder := range placeholders {
+		log.Printf("Importing resource %d of %d", i+1, len(placeholders))
+		if err := placeholder.ImportCommand.Run(); err != nil {
+			log.Fatal("Problem executing terraform import", placeholder.ImportCommand.Args, err)
 		}
 	}
+	return readTFStateJSON()
 }
 
-func writeInitialMainTF(f *os.File, resources []TerraformResource) {
-	var contents []byte
-	for _, resource := range resources {
-		contents = append(contents, resource.Content...)
-	}
-	f.Write(contents)
-}
-
-func createResourceRepresentation(app models.App) TerraformResource {
-	resource := TerraformResource{
-		Resource: app,
-	}
-	// todo: this would be better off as a config file or redis lookup
-	switch *app.AuthMethod {
-	case 1:
-		resource.Type = "onelogin_oidc_apps"
-	case 2:
-		resource.Type = "onelogin_saml_apps"
-	default:
-		resource.Type = "onelogin_apps"
-	}
-	resource.Name = fmt.Sprintf("%s-%d", toSnakeCase(replaceSpecialChar(*app.Name, "")), *app.ID)
-	resource.CreateResourceFileOutput()
-	resource.CreateTerraformImportCommand()
-	return resource
-}
-
-func resourceBaseToHCL(input interface{}) []byte {
+func resourceBaseToHCL(input interface{}, indentLevel int) []byte {
 	var out []byte
 
 	tp := reflect.TypeOf(input)
 	vl := reflect.ValueOf(input)
 
 	for i := 0; i < tp.NumField(); i++ {
-		q := vl.Field(i)
-		if !q.IsZero() {
-			switch q.Kind() {
+		line := make([]byte, indentLevel)
+		for i := 0; i < indentLevel; i++ {
+			line[i] = byte('\t')
+		}
+
+		field := vl.Field(i)
+		if !field.IsZero() {
+			switch field.Kind() {
 			case reflect.Ptr:
-				switch q.Elem().Kind() {
+				switch field.Elem().Kind() {
 				case reflect.String:
-					out = append(out, []byte(fmt.Sprintf("\t%s = \"%s\"\n", toSnakeCase(tp.Field(i).Name), q.Elem()))...)
+					line = append(line, []byte(fmt.Sprintf("%s = \"%s\"\n", toSnakeCase(tp.Field(i).Name), field.Elem()))...)
+					out = append(out, line...)
 				case reflect.Bool, reflect.Int, reflect.Int32, reflect.Int64:
-					out = append(out, []byte(fmt.Sprintf("\t%s = %v\n", toSnakeCase(tp.Field(i).Name), q.Elem()))...)
+					line = append(line, []byte(fmt.Sprintf("%s = %v\n", toSnakeCase(tp.Field(i).Name), field.Elem()))...)
+					out = append(out, line...)
 				default:
+					fmt.Println("Unable to Determine Type")
+				}
+			case reflect.Array, reflect.Slice:
+				for j := 0; j < field.Len(); j++ {
+					out = append(out, []byte(strings.ToLower(fmt.Sprintf("\n\t%s {\n", tp.Field(i).Name)))...)
+					out = append(out, resourceBaseToHCL(field.Index(j).Interface(), indentLevel+1)...)
+					out = append(out, []byte("\t}\n")...)
 				}
 			}
 		}
 	}
+
 	return out
 }
 
@@ -165,14 +235,13 @@ func getAllApps() []models.App {
 	return allApps
 }
 
-func setUpTerraformFile() *os.File {
+func setUpTerraformFile(name string) *os.File {
 	path, _ := os.Getwd()
-	p := filepath.Join(path, "/main.tf")
+	p := filepath.Join(path, fmt.Sprintf("/%s.tf", name))
 	f, err := os.Create(p)
 	if err != nil {
 		log.Fatal("ERROR CREATING FILE", err)
 	}
-	f.Write([]byte("provider onelogin {}\n\n"))
 	return f
 }
 
